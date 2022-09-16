e137ccd131352cb8c5c84e54fdd20ac08cc2c9cf
Galih Wahyu Pambudi <galihwahyup@student.uns.ac.id>
2022-09-16 15:32:23 +0700

    challenge promise

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+  const mergedData = [...ixx, ...vgc];
+
+  let countEmosi = 0;
+  mergedData.forEach(data => {
+    if (data.hasil === emosi) {
+      countEmosi += 1;
+    }
+  });
+  return countEmosi;
+};
 
 module.exports = {
   promiseOutput,
